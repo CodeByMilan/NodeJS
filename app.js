a30410f2037a9659df16619cca1af5f409b569f1
“CodeByMilan” <milanacharya1614@gmail.com>
2024-12-21 16:00:53 +0545

    added footer section

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const PORT=3000
 //to render frontend ejs template engine is used
 app.set('view engine', 'ejs')
 
@@ -18,6 +18,6 @@ app.get('/login',(req,res)=>{
     })
 
 app.use(express.static('public/css/'))
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+app.listen(PORT,()=>{
+    console.log('Server is running on port ',PORT)
 })
